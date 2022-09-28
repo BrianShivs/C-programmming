0debf7b994a7c010621238c5545cd0217351c3b3
BrianShivs <113585693+BrianShivs@users.noreply.github.com>
2022-09-28 13:58:43 +0300

    Add files via upload

---
+++ voting program.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    
+int age;
+   
+ //The program prompts for age 
+cout<<"enter your age"<<endl;
+    
+cin>>age;
+    
+if (age>=18)
+{
+    
+//The program tells you if you can vote or not
+cout<<"you are eligible to vote"<<endl;
+    
+}
+   
+else if (age<18){
+        
+cout<<"you are not eligible to vote"<<endl;
+  
+}
+    
+
+
+return 0;
+
+}
